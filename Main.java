918774e724ac9b87c7583ed2a41fca3ee03a648d
MateuszPisula01 <126687627+MateuszPisula01@users.noreply.github.com>
2023-03-15 08:56:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+
+    // && AND
+    // ||
+    int a=5 , b =6;
+    boolean logiczna=a<b || 5<=4; 
+    System.out.println(logiczna);
+  }
+}
